835e77b5e186630511d0cb7a52e8c729009b4c4f
Brittany <bfram2@uis.edu>
2016-12-02 00:15:00 -0600

    Add files via upload

--- cards/Chance.java
+++ cards/Chance.java
@@ -1,53 +1,49 @@
 package cards;
-import java.util.Random;
-//import pieces.*;
-//import events.Players;
-//For image Chance Cards and their events
+import events.Players;
 
 	public class Chance {
-		int PrevPos = 0; //player's current position
-		int adjust = 0; //player's current balance
 		int Outjail = 0; //get out of jail counter
 		int jail = 0;    //in jail, cannot pass go if(j=0)
 		int chanceno;
 		String imgname;
 		String outcome;
-		Random chan = new Random();
 		
-		public void setPrevPos(int PrevPos) {this.PrevPos = PrevPos;}
-		
-		public Chance () {
-			pull(); //pick card
+		public Chance (Players p) {
+			pull(p); //pick card
 		}
-		public Chance(int chanceno, String imgname, String outcome, int adjust) {
+		public Chance(int chanceno, String imgname, String outcome) {
 			chanceno = this.chanceno; //chanceno for random rolling 1-16
 			imgname = this.imgname; //imgname is the image location
-			adjust = this.adjust; //adjust is the amount Bal will change by
 			outcome = this.outcome; //prints the text of the card
 		}
-		public void pull() {
-		chanceno = chan.nextInt(16) + 1;
+		public void pull(Players play) {
+		Players thePlayer = play;
+		
+		chanceno = (int)(Math.random()*16) + 1;
 		imgname = "src/cards/images/Chance"+chanceno+".PNG"; // src/cards/images/Chance1-16.PNG
-		if (jail == 0) {
 			if (chanceno == 1) {
-					if (PrevPos > 5) {adjust += 200;} //pass go
-					PrevPos = 5; //Via Appia
+					if (thePlayer.getPosition()> 5) {
+					int changeBalance = thePlayer.getBalance() + 200;
+					thePlayer.setBalance(changeBalance);
+					} //pass go
+					thePlayer.setPosition(5); //Via Appia
 					outcome = "Take a trip to Via Appia. If you pass Rome collect 200 denarius.";
 			}
 			if (chanceno == 2 || chanceno == 3) {
 				outcome = "Advance to the nearest Via. If UNOWNED, you may buy it from the Bank. If OWNED, pay owner twice the rental to which they are otherwise entitled.";
-				if (PrevPos > 35 || PrevPos < 5) {
-					PrevPos = 5; //Via Appia
-					adjust += 200; //pass go
+				if (thePlayer.getPosition() > 35 || thePlayer.getPosition() < 5) {
+					thePlayer.setPosition(5); //Via Appia
+					int changeBalance = thePlayer.getBalance() + 200;
+					thePlayer.setBalance(changeBalance);
 				}
-				if (PrevPos > 4 || PrevPos < 16) {
-					PrevPos = 16; //Via Flaminia
+				if (thePlayer.getPosition() > 4 || thePlayer.getPosition() < 16) {
+					thePlayer.setPosition(16); //Via Flaminia
 				}
-				if (PrevPos > 15 || PrevPos < 26) {
-					PrevPos = 26; //Via Aemilia
+				if (thePlayer.getPosition() > 15 || thePlayer.getPosition() < 26) {
+					thePlayer.setPosition(26); //Via Aemilia
 				}
-				if (PrevPos > 25 || PrevPos < 36) {
-					PrevPos = 36; //Via Popillia
+				if (thePlayer.getPosition()> 25 || thePlayer.getPosition() < 36) {
+					thePlayer.setPosition(36); //Via Popillia
 				}
 			} //nearest Via, if owned: pay 2(rent), if unowned can buy
 			if (chanceno == 4) {
@@ -55,75 +51,82 @@ import java.util.Random;
 				Outjail++; //get out of jail for free
 			}
 			if (chanceno == 5) {
-				
 				outcome = "Advance to the nearest Utility. If UNOWNED, you may buy it from the Bank. If OWNED, throw dice and pay owner a total ten times amount thrown.";
-				if (PrevPos > 28 || PrevPos < 13) {
-					PrevPos = 13; //sewers
+				if (thePlayer.getPosition() > 28 || thePlayer.getPosition() < 13) {
+					thePlayer.setPosition(13); //sewers
 				}
-				if (PrevPos > 12 || PrevPos < 29) {
-					PrevPos = 29; //aqueducts
+				if (thePlayer.getPosition() > 12 || thePlayer.getPosition() < 29) {
+					thePlayer.setPosition(29); //aqueducts
 				} 
 			} //utilities
 			if (chanceno == 6) {
-				
 				outcome = "Advance to Rome. Collect 200 denarius.";
-				PrevPos = 0;
-				adjust += 200;
+				thePlayer.setPosition(0);
+				int changeBalance = thePlayer.getBalance() + 200;
+				thePlayer.setBalance(changeBalance);
 			} //go to Rome
 			if (chanceno == 7) {
 				outcome = "A new road is built near your Villa raising its value, collect 150 denarius.";
-				adjust += 150; //collect 150
+				int changeBalance = thePlayer.getBalance() + 150;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chanceno == 8) {
 				outcome = "Go back 3 spaces.";
-				PrevPos -= 3; //go back 3 spaces
+				int newPosition = thePlayer.getPosition() - 3;
+				thePlayer.setPosition(newPosition);
 			}
 			if (chanceno == 9) {
 				outcome = "Make general repairs on all of your properties: For villa pay 25 denarius, For pantheons pay 100 denarius. If no villas or pantheons are owned pay 100 denarius.";
-				adjust -= 100; //a fine for now
-				//Make general repairs on all of your properties:
-				//player's villas times 25
-				//player's pantheons times 100
+				int changeBalance = thePlayer.getBalance() - 100;
+				thePlayer.setBalance(changeBalance);
+				//a fine for now
+				//Make general repairs on all of your properties: player's villas times 25, player's pantheons times 100
 			}
 			if (chanceno == 10) {
 				outcome = "Advance to Italia.";
-				PrevPos = 40; //go to Italia
+				thePlayer.setPosition(40); //go to Italia
 			}
 			if (chanceno == 11) {
 				outcome = "Recent investment in more merchant carts gains you 50 denarius.";
-				adjust += 50; //collect 50
+				int changeBalance = thePlayer.getBalance() + 50;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chanceno == 12) {
 				outcome = "Your horses escape into the city. Pay a 15 denarius fine.";
-				adjust -= 15; //fine of 15
+				int changeBalance = thePlayer.getBalance() - 15;
+				thePlayer.setBalance(changeBalance);//fine of 15
 			}
 			if (chanceno == 13) {
 				outcome = "Go to the Gladiatorial Arena. Go directly to the arena, DO NOT pass Rome, DO NOT collect 200 denarius.";
-				PrevPos = 11;
+				thePlayer.setPosition(11);
 				jail = 1; //go to jail
 			}
 			if (chanceno == 14) {
 				outcome = "Advance to Aquitania. If you pass Rome, Collect 200 denarius.";
-					if (PrevPos > 12) {adjust += 200;} //pass go
-					PrevPos = 12; //Aquitania
+					if (thePlayer.getPosition() > 12) {
+						int changeBalance = thePlayer.getBalance() + 200;
+						thePlayer.setBalance(changeBalance);
+						} //pass go
+					thePlayer.setPosition(12); //Aquitania
 			}
 			if (chanceno == 15) {
 				outcome = "Advance to Cappadocia. If you pass Rome, Collect 200 denarius.";
-					if (PrevPos > 25) {adjust += 200;} //pass go
-					PrevPos = 25; //Cappadocia
+					if (thePlayer.getPosition() > 25) {
+						int changeBalance = thePlayer.getBalance() + 200;
+						thePlayer.setBalance(changeBalance);
+						}//pass go
+					thePlayer.setPosition(25);  //Cappadocia
 			}
 			if (chanceno == 16) {
 				outcome = "You have been elected a Senator, pay each player 50 denarius.";
-				adjust -= 50; //fine of 50
+				int changeBalance = thePlayer.getBalance() - 50;
+				thePlayer.setBalance(changeBalance);//fine of 50
 			} //pay each player 50 later, count Players array
-		 } //can't be in jail
 		}  //end pull
 		public int getChanceNo() {return chanceno;}
 		public String getImgName() {return imgname;} 
 		public String getOutcome() {return outcome;}
-		public int getAdjust() {return adjust;}
 		public int getOutJail() {return Outjail;}
-		public int getPrevPos() {return PrevPos;}
 }
 	
 	/*
--- cards/CommunityChest.java
+++ cards/CommunityChest.java
@@ -1,102 +1,132 @@
 package cards;
 import java.util.Random;
+
+import events.Players;
+import tile.Properties;
 //For Community Chest Cards & images
 //import events.Players;
 
 public class CommunityChest {
-	int PrevPos = 0; //player's current position
 	int Outjail = 0; //get out of jail counter
-	int jail = 0;    //in jail, cannot pass go if(j=0)
 	int chestno;
 	String imgname;
-	int adjust;
 	String outcome;
 	Random ches = new Random();
 	
-	public CommunityChest() {
-		heartofcards(); //pick card
+	public CommunityChest(Players p, Players [] play, Properties [] prop) {
+		heartofcards(p, play, prop); //pick card
+		
 	}
-	public CommunityChest(int chestno, String imgname, String outcome, int adjust) {
+	public CommunityChest(int chestno, String imgname, String outcome) {
 		this.chestno = chestno;
 		this.imgname = imgname;
 		this.outcome = outcome;
-		this.adjust = adjust;
 	}
-	public void heartofcards() {
-		chestno = ches.nextInt(16) + 1;
+	public void heartofcards(Players p, Players [] play, Properties [] prop) {
+		Players thePlayer = p;
+		
+
+		chestno = (int)(Math.random()*16) + 1;
 		imgname = "src/cards/images/Chest"+chestno+".PNG"; // src/cards/images/Chest1-16.PNG
-		if (jail == 0) {
 			//chestno 1-16
 			if (chestno == 1) {
 				outcome = "Bank error in your favor. Collect 200 denarius.";
-				adjust += 200;
+				int changeBalance = thePlayer.getBalance() + 200;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 2) {
 				outcome = "Pay private physician fees of 100 denarius.";
-				adjust -= 100;
+				int changeBalance = thePlayer.getBalance() - 100;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 3) {
 				outcome = "From sale of merchant carts, you get 50 denarius.";
-				adjust += 50;
+				int changeBalance = thePlayer.getBalance() + 50;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 4) {	
 				outcome = "You inherit a small Villa, gain 100 denarius.";
-				adjust += 100;
+				int changeBalance = thePlayer.getBalance() + 100;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 5) {
-				jail = 1;
 				outcome = "Go to the Gladiatorial Arena. Go directly to the arena, DO NOT pass Rome, DO NOT collect 200 denarius.";
+				thePlayer.setPosition(11);
+				thePlayer.setJailCounter(1);
 			}
 			if (chestno == 6) {
-				PrevPos = 0;
+				thePlayer.setPosition(0);
 				outcome = "Advance to Rome. Collect 200 denarius.";
-				adjust += 200;
+				int changeBalance = thePlayer.getBalance() + 200;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 7) {
 				outcome = "Render too much unto Caesar, Collect 200 denarius.";
-				adjust += 200;
+				int changeBalance = thePlayer.getBalance() + 200;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 8) {
 				outcome = "Roman official assesses you for street repairs: 40 per villa, 115 per pantheon. No villas or pantheons pay 100 denarius";
-				adjust -= 100; //needs to be: 40(villa), 115(pantheon)
+				int villas = 0;
+				int pantheon = 0;
+				for(int i = 0; i < prop.length; i++){
+					if(prop[i].getOwner()== thePlayer.getPlayerNumber()){
+						villas += prop[i].getVillas();
+						pantheon += prop[i].getPantheons();
+					}
+				}
+				int changeBalance = thePlayer.getBalance() + (villas * 40) + (pantheon * 115);
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 9) {
 				outcome = "You have won second place in the Campus Track & Field foot race. Collect 10 denarius.";
-				adjust += 10;
+				int changeBalance = thePlayer.getBalance() + 10;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 10) {
 				outcome = "Your properties and assets mature, join the Equestrian Order. Collect 100 denarius.";
-				adjust += 100;
+				int changeBalance = thePlayer.getBalance() + 100;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 11) {
 				outcome = "Sell bulk staples to the bank, receive 100 denarius.";
-				adjust += 100;
+				int changeBalance = thePlayer.getBalance() + 100;
+				thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 12) {
 			outcome = "Pay farmer's tax of $25.";
-			adjust -= 25;
+			int changeBalance = thePlayer.getBalance() - 25;
+			thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 13) {
 			outcome = "You have a coming of age party, Collect 10 denarius from every player.";
-			adjust += 10; //needs to be per player array
+			for(int i = 0; i < play.length; i++){
+				if(play[i].getCurrentPlayer() == false){
+					int add = thePlayer.getBalance() + 10;
+					int subtract = play[i].getBalance() - 10;
+					thePlayer.setBalance(add);
+					play[i].setBalance(subtract);
+				}
+			}
 			}
 			if (chestno == 14) {
 			outcome = "Pay school fees of 50 denarius.";
-			adjust -= 50;
+			int changeBalance = thePlayer.getBalance() - 50;
+			thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 15) {
 			outcome = "Medical institution fees, pay 50 denarius.";
-			adjust -= 50;
+			int changeBalance = thePlayer.getBalance() - 50;
+			thePlayer.setBalance(changeBalance);
 			}
 			if (chestno == 16) {
 				Outjail++;
+				thePlayer.setOutJail(Outjail);
 				outcome = "Gain a favor with a Senator, get out of the Gladiatorial Area for free. This card may be kept until needed or traded.";
 			}
 		}
-	}
 	public int getChestNo() {return chestno;}
 	public String getImgName() {return imgname;} 
 	public String getOutcome() {return outcome;}
-	public int getAdjust() {return adjust;}
 	public int getOutJail() {return Outjail;}
 }
